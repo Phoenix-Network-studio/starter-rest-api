02be3415eee97bacaff8b75199fa3e0b93ffa891
Phoenix-Network-studio <123342183+Phoenix-Network-studio@users.noreply.github.com>
2023-06-15 21:43:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,115 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Beispiel-Datenbank als JavaScript-Array
-let books = [
-  { id: 1, title: "Harry Potter and the Philosopher's Stone", author: "J.K. Rowling" },
-  { id: 2, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
-  { id: 3, title: "To Kill a Mockingbird", author: "Harper Lee" }
-];
+// API-Schlüssel zum Vergleich
+const API_KEY = 'your-api-key';
 
-app.use(bodyParser.json());
+// Middleware-Funktion für die API-Schlüsselüberprüfung
+const checkApiKey = (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
 
-// API-Endpunkt zum Abrufen aller Bücher
-app.get('/books', (req, res) => {
-  res.json(books);
+  if (!apiKey || apiKey !== API_KEY) {
+    return res.status(401).json({ message: 'Invalid API key' });
+  }
+
+  next();
+};
+
+// Verbindung zur MongoDB-Datenbank
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// API-Endpunkt zum Abrufen eines einzelnen Buches anhand der ID
-app.get('/books/:id', (req, res) => {
-  const bookId = parseInt(req.params.id);
-  const book = books.find(book => book.id === bookId);
-  if (book) {
-    res.json(book);
-  } else {
-    res.status(404).json({ message: 'Book not found' });
+// Definieren Sie das Datenmodell für die Entität "Item"
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// API-Schlüsselüberprüfung für alle Routen
+app.use(checkApiKey);
+
+// GET: Alle Elemente abrufen
+app.get('/api/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
-// API-Endpunkt zum Hinzufügen eines neuen Buches
-app.post('/books', (req, res) => {
-  const { id, title, author } = req.body;
-  const newBook = { id, title, author };
-  books.push(newBook);
-  res.status(201).json(newBook);
+// GET: Einzelnes Element abrufen
+app.get('/api/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (item) {
+      res.json(item);
+    } else {
+      res.status(404).json({ message: 'Item not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-// API-Endpunkt zum Aktualisieren eines Buches anhand der ID
-app.put('/books/:id', (req, res) => {
-  const bookId = parseInt(req.params.id);
-  const updatedBook = req.body;
-  books = books.map(book => (book.id === bookId ? updatedBook : book));
-  res.json(updatedBook);
+// POST: Neues Element erstellen
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    name: req.body.name,
+    description: req.body.description,
+  });
+
+  try {
+    const newItem = await item.save();
+    res.status(201).json(newItem);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-// API-Endpunkt zum Löschen eines Buches anhand der ID
-app.delete('/books/:id', (req, res) => {
-  const bookId = parseInt(req.params.id);
-  books = books.filter(book => book.id !== bookId);
-  res.sendStatus(204);
+// PUT: Vorhandenes Element aktualisieren
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      description: req.body.description,
+    });
+    if (item) {
+      res.json(item);
+    } else {
+      res.status(404).json({ message: 'Item not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// DELETE: Ein Element löschen
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findByIdAndDelete(req.params.id);
+    if (item) {
+      res.json({ message: 'Item deleted' });
+    } else {
+      res.status(404).json({ message: 'Item not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Server starten
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
